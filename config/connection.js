02410b861a1686bdd32466805c1e8302088a167d
kmvanneste <kmvanneste17@gmail.com>
2020-08-31 09:47:16 -0400

    updating for heroku connection

--- config/connection.js
+++ config/connection.js
@@ -17,7 +17,7 @@ if (process.env.JAWSDB_URL) {
 //   host: "localhost",
 //   port: 3306,
 //   user: "root",
-//   password: ,
+//   password: process.env.MYSQL_KEY,
 //   database: "sushi_db"
 // });
 
